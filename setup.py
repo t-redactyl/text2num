f989ccb71a0416a120619ebfa4934c4d70c17baa
Jodie Burchell <jodie.burchell@gmail.com>
2017-04-16 17:16:33 +1000

    Adding files to pip this package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='text2num',
+    version='0.1.0',
+    description='A simple implementation of a function to convert textual numbers written in English into their integer representations.',
+    url='https://github.com/t-redactyl/text2num',
+    license='MIT',
+    author='Greg Hewgill',
+    author_email='greg@hewgill.com',
+    py_modules=['text2num'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
